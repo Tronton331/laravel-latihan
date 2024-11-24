8af506d45fda8f9d0754fef1a27864ca411b4c82
remove <tronton331@gmail.com>
2024-11-24 20:10:34 +0700

    login register (redesign)

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,35 +7,86 @@
     <title>Login</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <style>
+        :root
+        {
+            --primary-color: #b5afaf;
+            --second-color: #b9b9b9;
+            --black-color: #020101;
+        }
+        *
+        {
+            padding: 0px;
+            margin: 0px;
+        }
         body
         {
-            background-color: beige;
+            background-color: rgb(174, 174, 174);
+            /* background-image: url(""); */
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-attachment: fixed;
+
             font-family: "Poppins", Arial, sans-serif;
-            margin: 0;
             height: 100vh;          /*  persen tinggi viewport  */
             width: 100vw;           /*  persen lebar viewport   */
             overflow: hidden;       /*  Hilangkan scroll        */
         }
         main
         {
-            background-color: cyan;
-            padding: 50px;
-            height: 340px;
-            width: 200px;
             /*  posisi di tengah */
-            position: absolute;                 /*  Mengapung bebas                 */
+            position: relative;                 /*  Mengapung bebas                 */
             top: 50%;                           /*  Bagian atas berjarak 50%        */
             left: 50%;                          /*  Bagian kiri berjarak 50%        */
             transform: translate(-50%, -50%);   /*  Tumpuan menjadi di engah object */
+
+            background-color: rgba(0, 0, 0, 0.2);
+            padding: 10px 50px;
+            height: 360px;
+            width: 200px;
+            border: 2px solid black;
+            border-radius: 30px;
         }
         h2
         {
             font-size: 24px;
             text-align: center;
+            padding-bottom: 20px;
         }
         p
         {
             font-size: 10px;
+            position: relative;
+            top: 100px;
+        }
+        a{
+            text-decoration: none;
+            color: blue;
+        }
+        a:hover
+        {
+            text-decoration: underline;
+        }
+        .input_text
+        {
+            height: 30px;
+            width: 100%;
+            border: 1px solid black;
+            border-radius: 20px;
+            background-color: rgba(0, 0, 0, 0.1)
+        }
+        .password
+        {
+            position: relative;
+            top: 10px;
+        }
+        .button
+        {
+            position: relative;
+            top: 40px;
+            padding: 10px 20px;
+            border: 1px solid;
+            border-radius: 20px;
         }
     </style>
 </head>
@@ -43,17 +94,18 @@
     <main>
         <h2 class="login">Login</h2>
         <form name="login">
-            <div class="user">
-                <label for="username">Username:</label><br>
-                <input type="text" name="username" id="username" required><br>
+            <div class="username">
+                <label for="username" class="user">Username:</label><br>
+                <input type="text" name="username" id="username" class="input_text" required><br>
             </div>
             <div class="password">
-                <label for="password">Password:</label><br>
-                <input type="password" name="password" id="password" required><br>
+                <label for="password" class="pass">Password:</label><br>
+                <input type="password" name="password" id="password" class="input_text" required><br>
             </div>
-            <input type="submit" value="Login">
+            <input type="submit" value="Login" class="button">
         </form>
-        <p>Tidak memiliki akun? <a href="{{ route('register')}}">Daftar sekarang</a></p>
+        {{-- <p>Tidak memiliki akun? <a href="{{ route('register')}}">Daftar sekarang</a></p> --}}
+        <p>Tidak memiliki akun? <a href="/">Daftar sekarang</a></p>
     </main>
 </body>
 </html>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,31 +6,91 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Register</title>
     <style>
-        body {
-            background-color: beige;
-            font-family: "Poppins", Arial, sans-serif;
-            margin: 0;
-            height: 100vh;      /*  persen tinggi viewport  */
-            width: 100vw;       /*  persen lebar viewport   */
-            overflow: hidden;   /*  Hilangkan scroll        */
+        :root
+        {
+        --primary-color: #b5afaf;
+        --second-color: #b9b9b9;
+        --black-color: #020101;
         }
-
-        main {
-            background-color: cyan;
-            padding: 50px;
-            width: 5%;
-            height: 50%;
-            min-width: 200px;
-            /*  posisi di tengah */
-            position: absolute;                 /*  Mengapung bebas                 */
-            top: 50%;                           /*  Bagian atas berjarak 50%        */
-            left: 50%;                          /*  Bagian kiri berjarak 50%        */
-            transform: translate(-50%, -50%);   /*  Tumpuan menjadi di engah object */
+        *
+        {
+        padding: 0px;
+        margin: 0px;
         }
+        body
+        {
+        background-color: rgb(174, 174, 174);
+        /* background-image: url(""); */
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-attachment: fixed;
 
-        h2 {
-            font-size: 64;
-            text-align: center;
+        font-family: "Poppins", Arial, sans-serif;
+        height: 100vh; /* persen tinggi viewport */
+        width: 100vw; /* persen lebar viewport */
+        overflow: hidden; /* Hilangkan scroll */
+        }
+        main
+        {
+        /* posisi di tengah */
+        position: relative; /* Mengapung bebas */
+        top: 50%; /* Bagian atas berjarak 50% */
+        left: 50%; /* Bagian kiri berjarak 50% */
+        transform: translate(-50%, -50%); /* Tumpuan menjadi di engah object */
+
+        background-color: rgba(0, 0, 0, 0.2);
+        padding: 10px 50px;
+        height: 360px;
+        width: 200px;
+        border: 2px solid black;
+        border-radius: 30px;
+        }
+        h2
+        {
+        font-size: 24px;
+        text-align: center;
+        padding-bottom: 20px;
+        }
+        p
+        {
+        font-size: 10px;
+        position: relative;
+        top: 100px;
+        }
+        a{
+        text-decoration: none;
+        color: blue;
+        }
+        a:hover
+        {
+        text-decoration: underline;
+        }
+        .input_text
+        {
+        height: 30px;
+        width: 100%;
+        border: 1px solid black;
+        border-radius: 20px;
+        background-color: rgba(0, 0, 0, 0.1)
+        }
+        .email
+        {
+        position: relative;
+        top: 10px;
+        }
+        .password
+        {
+        position: relative;
+        top: 20px;
+        }
+        .button
+        {
+        position: relative;
+        top: 40px;
+        padding: 10px 20px;
+        border: 1px solid;
+        border-radius: 20px;
         }
     </style>
 </head>
@@ -40,17 +100,17 @@
         <form name="register">
             <div class="name">
                 <label for="name">Name</label>
-                <input type="text" name="name" id="name"><br>
+                <input type="text" name="name" id="name" class="input_text"><br>
             </div>
             <div class="email">
                 <label for="email">Email</label>
-                <input type="email" name="email" id="email"><br>
+                <input type="email" name="email" id="email" class="input_text"><br>
             </div>
             <div class="password">
                 <label for="password">Password</label>
-                <input type="password" name="password" id="password"><br>
+                <input type="password" name="password" id="password" class="input_text"><br>
             </div>
-            <input type="submit" value="register">
+            <input type="submit" value="register" class="button">
         </form>
     </main>
 </body>
